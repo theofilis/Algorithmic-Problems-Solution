995513e20658a5e8d5083b5e5f090105ccf827b8
George Theofilis <theofilis.g@gmail.com>
2021-12-14 01:16:06 +0200

    Init first day

---
+++ Algorithms/Extensions/InputExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Algorithms.Extensions;
+
+public static class InputExtensions
+{
+    public static int[] ToIntegerArray(this string input) =>
+        input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+}
